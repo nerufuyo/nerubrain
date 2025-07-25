455e02dc24e5a9dbbe356591a3294cb72532b7b6
nerufuyo <listyoadipamungkas1401@gmail.com>
2025-07-25 09:33:00 +0700

    chore: Add Jest configuration backup files for development reference

---
+++ jest.config.new.js
@@ -0,0 +1,37 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: [
+    '**/tests/**/*.test.ts',
+    '**/src/**/*.test.ts',
+    '**/__tests__/**/*.ts'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 100,
+      statements: 100
+    }
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1'
+  },
+  testTimeout: 10000,
+  verbose: true
+};
